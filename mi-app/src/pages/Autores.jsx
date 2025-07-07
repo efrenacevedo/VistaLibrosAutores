eda35314e1fc3658f0521983e1733a9457a4c5c1
Efren <joseefrenacevedo3@gmail.com>
2025-07-07 16:34:06 -0600

    Tu mensaje descriptivo aquí

--- mi-app/src/pages/Autores.jsx
+++ mi-app/src/pages/Autores.jsx
@@ -10,7 +10,7 @@ import {
   Search as SearchIcon
 } from '@mui/icons-material';
 
-const API_BASE = 'https://tiendamicroserviciosautorapi-production.up.railway.app/api/autores'; // Cambia por tu base URL
+const API_BASE = 'http://www.autorlibrosservices.somee.com/api/autores'; 
 
 const Autores = () => {
   const [autores, setAutores] = useState([]);
