0202162dbcc2b093c2bc77516dd2dda8f12dd6e0
Efren <joseefrenacevedo3@gmail.com>
2025-07-09 14:55:14 -0600

    cambio

--- mi-app/src/services/api.js
+++ mi-app/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ine-node-micro-s.vercel.app/api", // Asegúrate que el backend corre en ese puerto
+  baseURL: "https://inenodemicros-production.up.railway.app/api", // Asegúrate que el backend corre en ese puerto
 });
 
 export default api;
