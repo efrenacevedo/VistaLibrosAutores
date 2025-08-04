676b970317d5996c6807759f5a4f459c10a3e286
Efren <joseefrenacevedo3@gmail.com>
2025-08-04 16:10:49 -0600

    Camb

--- mi-app/src/services/api.js
+++ mi-app/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://inenodemicros-production.up.railway.app/api", // Asegúrate que el backend corre en ese puerto
+  baseURL: "https://ine-node-micro-s-pwq4.vercel.app/api", // Asegúrate que el backend corre en ese puerto
 });
 
 // Middleware para insertar token si existe
